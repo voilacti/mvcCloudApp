342389c699a060ef29e96a8c4bb040cb7dda4484
Alexandre <alexandrehervier1995@gmail.com>
2017-11-06 22:01:28 +0100

    [AHR] Setup basic mvcApp

---
+++ mvcCloudApp/src/com/sun/servlets/CreationClient.java
@@ -0,0 +1,47 @@
+package com.sun.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.*;
+
+import com.sun.beans.Client;
+import com.sun.forms.CreationClientForm;
+
+public class CreationClient extends HttpServlet {
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public static final String ATT_CLIENT = "client";
+    public static final String ATT_FORM   = "form";
+
+    public static final String VUE_SUCCES = "/WEB-INF/afficherClient.jsp";
+    public static final String VUE_FORM   = "/WEB-INF/creerClient.jsp";
+
+    public void doGet( HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException {
+        /* À la réception d'une requête GET, simple affichage du formulaire */
+        this.getServletContext().getRequestDispatcher( VUE_FORM ).forward( request, response );
+    }
+
+    public void doPost( HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException {
+        /* Préparation de l'objet formulaire */
+        CreationClientForm form = new CreationClientForm();
+
+        /* Traitement de la requête et récupération du bean en résultant */
+        Client client = form.creerClient( request );
+
+        /* Ajout du bean et de l'objet métier à l'objet requête */
+        request.setAttribute( ATT_CLIENT, client );
+        request.setAttribute( ATT_FORM, form );
+
+        if ( form.getErreurs().isEmpty() ) {
+            /* Si aucune erreur, alors affichage de la fiche récapitulative */
+            this.getServletContext().getRequestDispatcher( VUE_SUCCES ).forward( request, response );
+        } else {
+            /* Sinon, ré-affichage du formulaire de création avec les erreurs */
+            this.getServletContext().getRequestDispatcher( VUE_FORM ).forward( request, response );
+        }
+    }
+}
---
+++ mvcCloudApp/src/com/sun/servlets/CreationCommande.java
@@ -0,0 +1,46 @@
+package com.sun.servlets;
+
+import java.io.IOException;
+import java.util.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.sun.beans.Client;
+import com.sun.beans.Commande;
+import com.sun.forms.CreationCommandeForm;
+
+public class CreationCommande extends HttpServlet {
+    public static final String ATT_COMMANDE = "commande";
+    public static final String ATT_FORM     = "form";
+
+    public static final String VUE_SUCCES   = "/WEB-INF/afficherCommande.jsp";
+    public static final String VUE_FORM     = "/WEB-INF/creerCommande.jsp";
+
+    public void doGet( HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException {
+        /* À la réception d'une requête GET, simple affichage du formulaire */
+        this.getServletContext().getRequestDispatcher( VUE_FORM ).forward( request, response );
+    }
+
+    public void doPost( HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException {
+        /* Préparation de l'objet formulaire */
+        CreationCommandeForm form = new CreationCommandeForm();
+
+        /* Traitement de la requête et récupération du bean en résultant */
+        Commande commande = form.creerCommande( request );
+
+        /* Ajout du bean et de l'objet métier à l'objet requête */
+        request.setAttribute( ATT_COMMANDE, commande );
+        request.setAttribute( ATT_FORM, form );
+
+        if ( form.getErreurs().isEmpty() ) {
+            /* Si aucune erreur, alors affichage de la fiche récapitulative */
+            this.getServletContext().getRequestDispatcher( VUE_SUCCES ).forward( request, response );
+        } else {
+            /* Sinon, ré-affichage du formulaire de création avec les erreurs */
+            this.getServletContext().getRequestDispatcher( VUE_FORM ).forward( request, response );
+        }
+    }
+}
